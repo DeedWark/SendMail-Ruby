81380206b917da8507d0d0017ad68a1375121e5d
DeedWark <deedwark@hotmail.fr>
2020-12-18 00:00:33 +0100

    Update SendMail.rb

--- SendMail.rb
+++ SendMail.rb
@@ -16,19 +16,19 @@ def usage()
   -p  	         Set TCP Port (default "25/SMTP")
   -f             Set MAIL FROM (protocolar)
   -t  	         Set RCPT TO (protocolar)
---hfrom          Set Header From (ex "Me <go@lang.org>")
---hto            Set Header To (ex "You <go@pher.org>")
+--hfrom          Set Header From (ex "Me <ruby@lang.org>")
+--hto            Set Header To (ex "You <ruby@gem.org>")
 --subject        Set a subject
 --date           Set a custom date (default "current date")
 --body           Write content to Body
 --attach         Add an attachment/file
 --auth           Enable authentication (Gmail, Outlook...)
---x-mailer       Set a custom X-Mailer (default "SendMail-Golang v2.0")
+--x-mailer       Set a custom X-Mailer (default "SendMail-Ruby v1")
 --x-priority     Set a custom X-Priority
 --charset        Set a custom charset (default "UTF-8")
 --html-file      Import a HTML file as body
 --text-file      Import a TXT file as body
---boundary       Set a custom boundary (default "------=_MIME_BOUNDARY_GOO_LANG--")
+--boundary       Set a custom boundary (default "------=_MIME_BOUNDARY_RUBY_LANG--")
 --content-type   Set a custom Content-Type (default "text/plain")
 --base64         Encode body in base64
 --prompt         Get a prompt to write on your terminal '
@@ -96,7 +96,8 @@ OptionParser.new do |parser|
         $xprio = xprio
     end
     parser.on("--boundary BOUNDARY", "Set a custom Boundary") do |boundary|
-        $boundary = boundary end
+        $boundary = boundary 
+    end
     parser.on("--content-type CONTENT-TYPE", "Set a custom Content-Type") do |ctype|
         $ctype = ctype
     end
